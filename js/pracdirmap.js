19eec1e8bec0b6d166673505850e7c8dd95df68a
Damian Davila <damiandavilag@gmail.com>
2022-04-06 17:13:33 -0400

    Initial commit

---
+++ js/pracdirmap.js
@@ -0,0 +1,345 @@
+/**
+ * @name PracDirMap
+ * @version version 1.0
+ * @author Damian Davila
+ * @fileoverview
+ * Displays list of global businesses on a Google Map.  Utilizes MarkerClusterer.js to cluster nearby businesses, based upon zoom level.
+ * Developed specifically for veterinary practitioners (hence the name) but generic to any business that fits the schema.
+ * Developed to extend Joomla Membership Pro component's Member List page, but doesn't depend on anything from that page other than path to avatars which is set in a constant and can be changed.
+ * @param {} practitioner_data Contains the array of practitioner name, address, etc info, plus the count of practitioners.
+ */
+
+var pdm_data = {};
+var pdm_markers = [];
+var pdm_circle = null;
+
+var MAP_EL_ID = 'map';
+var MARKER_LIST_EL_ID = '#markerlist';
+var MAP_SEARCH_INPUT_EL_ID = 'pac-input';
+var MAP_SEARCH_RADIUS_EL_ID = 'pac-radius';
+
+const AVATAR_PATH = '/media/com_osmembership/avatars/';
+// Can use custom marker images (e.g. replacements for pin) on a per-marker basis.  
+// Just specify the directory path here and the file name in the data field 'profile_marker_picture'.
+const MARKER_IMAGE_PATH = '';
+const MARKER_DEFAULT_PATH = '/templates/aatcvm/images/';
+
+function showPractitionerMap(practitioner_data, map_el_id = MAP_EL_ID, marker_list_el_id = MARKER_LIST_EL_ID, map_search_input_el_id = MAP_SEARCH_INPUT_EL_ID, map_search_radius_el_id = MAP_SEARCH_RADIUS_EL_ID) {
+
+    pdm_data = practitioner_data;
+    MAP_EL_ID = map_el_id;
+    MARKER_LIST_EL_ID = marker_list_el_id;
+    MAP_SEARCH_INPUT_EL_ID = map_search_input_el_id;
+    MAP_SEARCH_RADIUS_EL_ID = map_search_radius_el_id;
+    buildMap();
+};
+
+function SortByName(x, y) {
+    return ((x.name == y.name) ? 0 : ((x.name > y.name) ? 1 : -1));
+}
+
+function buildMap(mar) {
+
+    if (mar == 1) {
+        pdm_markers = [];
+    }
+
+    jQuery(MARKER_LIST_EL_ID).html('');
+    var geocoder = new google.maps.Geocoder();
+    var bounds = new google.maps.LatLngBounds();
+    var center = new google.maps.LatLng(25, 0);
+    var mapOptions = {
+        mapTypeId: 'roadmap',
+        center: center,
+        zoom: 1.7,
+    };
+    map = new google.maps.Map(document.getElementById(MAP_EL_ID), mapOptions);
+
+    // Create the search box and link it to the UI element.
+    var input = document.getElementById(MAP_SEARCH_INPUT_EL_ID);
+    var searchBox = new google.maps.places.SearchBox(input);
+    //         map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    map.setCenter(center);
+
+    // Add multiple markers to map
+    var infoWindow = new google.maps.InfoWindow(),
+        marker, i;
+    var marker_new = [];
+    // Call Sort By Name
+    pdm_data.data.sort(SortByName);
+
+    for (var i = 0; i < pdm_data.member_count; i++) {
+        var dataPhoto = pdm_data.data[i];
+
+        if (dataPhoto) {
+
+            if (dataPhoto.profile_marker_picture) {
+                var image_icon = MARKER_IMAGE_PATH + dataPhoto.profile_marker_picture;
+            } else {
+                var image_icon = MARKER_DEFAULT_PATH + 'pin.png';
+            }
+
+            var latLng = new google.maps.LatLng(dataPhoto.latitude, dataPhoto.longitude);
+            var marker = new google.maps.Marker({
+                position: latLng,
+                icon: image_icon,
+            });
+
+            bounds.extend(marker.getPosition());
+
+            var panel = jQuery(MARKER_LIST_EL_ID);
+            panel.innerHTML = '';
+
+            var titleText = pdm_data.data[i].name;
+            if (titleText === '') {
+                titleText = 'No title';
+            }
+
+            var item = document.createElement('DIV');
+            var title = document.createElement('A');
+            title.href = '#';
+            title.className = 'title';
+            title.innerHTML = titleText;
+
+            item.appendChild(title);
+            jQuery(MARKER_LIST_EL_ID).append(item);
+
+            var fn = markerClickFunction(pdm_data.data[i], latLng, infoWindow);
+
+            google.maps.event.addDomListener(title, 'click', fn);
+
+            // Add info window to marker    
+            google.maps.event.addListener(marker, 'click', (function(marker, i) {
+
+                var str = pdm_data.data[i].member_link;
+                var info_html =
+                    '<div class="info">' +
+                    '<h3>' +
+                    pdm_data.data[i].name +
+                    '</h3>';
+                let logo_img = pdm_data.data[i].avatar ? '<img class="map_logo" width="50" height="auto" src="' + AVATAR_PATH + pdm_data.data[i].avatar + '">' : '';
+                info_html +=
+                    logo_img + '<br/>' +
+                    pdm_data.data[i].organization + '<br/>' +
+                    pdm_data.data[i].email + '<br/>' +
+                    pdm_data.data[i].address + '<br/><br/>' +
+                    '<a href=' + str + '>Read the full profile</a>' +
+                    '</div>';
+
+                return function() {
+                    infoWindow.setContent(info_html);
+                    infoWindow.open(map, marker);
+                }
+            })(marker, i));
+
+            pdm_markers.push(marker);
+
+        }
+    }
+    /*     
+        map.fitBounds(bounds);*/
+
+    map.setCenter(center);
+
+    var viewport_array = [];
+
+    // Bias the SearchBox results towards current map's viewport.
+    map.addListener('bounds_changed', function() {
+        searchBox.setBounds(map.getBounds());
+
+        var viewport_array = [];
+
+        for (var i = 0; i < pdm_markers.length; i++) {
+
+            if (map.getBounds().contains(pdm_markers[i].getPosition())) {
+                viewport_array.push(pdm_data.data[i]);
+            }
+        }
+
+        if (viewport_array.length > 0) {
+            jQuery(MARKER_LIST_EL_ID).html('');
+            for (var j = 0; j < viewport_array.length; j++) {
+
+                var latLng = new google.maps.LatLng(viewport_array[j].latitude,
+                    viewport_array[j].longitude);
+
+                var panel = jQuery(MARKER_LIST_EL_ID);
+                panel.innerHTML = '';
+
+                var titleText = viewport_array[j].name;
+                if (titleText === '') {
+                    titleText = 'No title';
+                }
+
+                var item = document.createElement('DIV');
+                var title = document.createElement('A');
+                title.href = '#';
+                title.className = 'title';
+                title.innerHTML = titleText;
+
+                item.appendChild(title);
+                jQuery(MARKER_LIST_EL_ID).append(item);
+
+                var fn = markerClickFunction(viewport_array[j], latLng, infoWindow);
+
+                google.maps.event.addDomListener(title, 'click', fn);
+
+            }
+
+        }
+
+
+
+    });
+
+    var markerCluster = new MarkerClusterer(map, pdm_markers, {
+        imagePath: '/images/map/m'
+    });
+}
+
+markerClickFunction = function(pic, latlng, infoWindow) {
+    return function(e) {
+        e.cancelBubble = true;
+        e.returnValue = false;
+
+        if (e.stopPropagation) {
+            e.stopPropagation();
+            e.preventDefault();
+        }
+        var title = pic.name;
+        var address = pic.address;
+        var email = pic.email;
+        var fileurl = pic.member_link;
+        var org = pic.organization;
+
+        var infoHtml = '<div class="info">' +
+            '<h3>' + title + '</h3>';
+        let logo_img = pic.avatar ? '<img class="map_logo" width="50" height="auto" src="' + AVATAR_PATH + pic.avatar + '"><br/>' : '';
+        infoHtml +=
+            logo_img +
+            '<div class="info-body">' + org + '</div><br/>' +
+            '<div class="info-body">' + email + '</div><br/>' +
+            '<div class="info-body">' + address + '</div><br/>' +
+            '<div class="info-body"><a href="' + fileurl + '" target="_blank">Read the full profile</a></div></div>';
+
+        if (pic.profile_marker_picture) {
+            var image_icon = MARKER_IMAGE_PATH + pic.profile_marker_picture;
+
+        } else {
+            var image_icon = MARKER_DEFAULT_PATH + 'pin.png';
+        }
+
+        var marker = new google.maps.Marker({
+            position: latlng,
+            icon: image_icon,
+        });
+
+        infoWindow.setContent(infoHtml);
+        infoWindow.setPosition(latlng);
+        infoWindow.open(map);
+
+        marker.setMap(map);
+        map.setZoom(15);
+        map.setCenter(latlng);
+    };
+};
+
+
+function codeAddress() {
+
+    var geocoder = new google.maps.Geocoder();
+
+    var address = document.getElementById(MAP_SEARCH_INPUT_EL_ID).value;
+
+    if (document.getElementById(MAP_SEARCH_RADIUS_EL_ID).value == '') {
+        alert("Please enter radius ");
+        return false;
+    }
+
+    var filter_users = [];
+    var radius = parseInt(document.getElementById(MAP_SEARCH_RADIUS_EL_ID).value, 10) * 1000;
+
+    geocoder.geocode({
+        'address': address
+    }, function(results, status) {
+
+        if (status == google.maps.GeocoderStatus.OK) {
+            side_bar_html = "";
+            map.setCenter(results[0].geometry.location);
+            var searchCenter = results[0].geometry.location;
+            if (pdm_circle) pdm_circle.setMap(null);
+            pdm_circle = new google.maps.Circle({
+                center: searchCenter,
+                radius: radius,
+                fillOpacity: 0.35,
+                fillColor: "#FF0000",
+                map: map
+            });
+            var bounds = new google.maps.LatLngBounds();
+            var foundMarkers = 0;
+
+            for (var i = 0; i < pdm_markers.length; i++) {
+                if (google.maps.geometry.spherical.computeDistanceBetween(pdm_markers[i].getPosition(), searchCenter) < radius) {
+
+                    filter_users.push(pdm_data.data[i]);
+
+                    bounds.extend(pdm_markers[i].getPosition())
+                    pdm_markers[i].setMap(map);
+                    // add a line to the side_bar html
+
+                    foundMarkers++;
+                } else {
+                    pdm_markers[i].setMap(null);
+                }
+            }
+
+            map.fitBounds(pdm_circle.getBounds());
+            // put the assembled side_bar_html contents into the side_bar div
+
+            var infoWindow = new google.maps.InfoWindow()
+
+            if (filter_users.length > 0) {
+                jQuery(MARKER_LIST_EL_ID).html('');
+                for (var j = 0; j < filter_users.length; j++) {
+
+                    var latLng = new google.maps.LatLng(filter_users[j].latitude,
+                        filter_users[j].longitude);
+
+                    var panel = jQuery(MARKER_LIST_EL_ID);
+                    panel.innerHTML = '';
+
+                    var titleText = filter_users[j].name;
+                    if (titleText === '') {
+                        titleText = 'No title';
+                    }
+
+                    var item = document.createElement('DIV');
+                    var title = document.createElement('A');
+                    title.href = '#';
+                    title.className = 'title';
+                    title.innerHTML = titleText;
+
+                    item.appendChild(title);
+                    jQuery(MARKER_LIST_EL_ID).append(item);
+
+                    var fn = markerClickFunction(filter_users[j], latLng, infoWindow);
+
+                    google.maps.event.addDomListener(title, 'click', fn);
+
+                }
+
+            }
+
+        } else {
+            alert('Geocode was not successful for the following reason: ' + status);
+        }
+    });
+}
+
+
+function reset_state() {
+    buildMap(mar = 1);
+    jQuery('#' + MAP_SEARCH_INPUT_EL_ID).val('');
+    jQuery('#' + MAP_SEARCH_RADIUS_EL_ID).val('');
+
+}
---
+++ views/osmembership/default.php
@@ -0,0 +1,286 @@
+<?php
+/**
+ * @package        Joomla
+ * @subpackage     OSMembership
+ * @author         Damian Davila
+ * @copyright      Copyright (C) 2022 Moventis, LLC
+ * @license        GNU/GPL, see LICENSE.php
+ */
+
+defined('_JEXEC') or die;
+JHtml::_('bootstrap.tooltip');
+
+/* @var OSMembershipViewMembersHtml $this */
+
+$showAvatar              = $this->params->get('show_avatar', 1);
+$showPlan                = $this->params->get('show_plan', 1);
+$showSubscriptionDate    = $this->params->get('show_subscription_date', 1);
+$showSubscriptionEndDate = $this->params->get('show_subscription_end_date', 0);
+$showLinkToProfile       = $this->params->get('show_link_to_detail', 0);
+$showMembershipId        = $this->params->get('show_membership_id', 0);
+
+$bootstrapHelper = $this->bootstrapHelper;
+$rowFluidClass   = $bootstrapHelper->getClassMapping('row-fluid');
+$clearfixClass   = $bootstrapHelper->getClassMapping('clearfix');
+$centerClass     = $bootstrapHelper->getClassMapping('center');
+
+$fields = $this->fields;
+
+// Remove first_name and last_name as it is displayed in single name field
+
+for ($i = 0, $n = count($fields); $i < $n; $i++)
+{
+	if (in_array($fields[$i]->name, ['first_name', 'last_name']))
+	{
+		unset($fields[$i]);
+	}
+}
+
+$cols    = count($fields);
+$rootUri = JUri::root(true);
+?>
+<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAOf2GKffM0r_T8-Ecbg4L-1no02RXux5k&libraries=geometry,places"></script>
+<script type="text/javascript" src="<?php echo JURI::base();?>templates/aatcvm/js/markerclusterer.js"></script>
+<script type="text/javascript" src="<?php echo JURI::base();?>templates/aatcvm/js/pracdirmap.js"></script>
+<script type="text/javascript">
+    window.addEventListener("load", function() {
+
+		// Trap the click on the member list search reset button.
+		// Note that the button markup has inline click handler, so must add click handler on parent in order to intercept the button click successfully.
+		var mbr_search_filter_container = document.querySelector('#osm-members-list div.filters');
+		var mbr_search_reset_button = document.querySelector('#osm-members-list div.filters button.btn:nth-child(2)');
+		var mbr_search_input = document.getElementById('filter_search');
+
+		mbr_search_filter_container.addEventListener("click", function(evt) {
+
+			if (mbr_search_reset_button.contains(evt.target)) {
+
+				if (mbr_search_input.value.length > 0) { 
+					// if text in field, then do normal process and re-do search
+					;
+				} else { 
+					evt.preventDefault();
+					evt.stopImmediatePropagation();
+					// otherwise, just reset the map because there's no prior database search to reset (probably)
+					document.querySelector('#osm-map input.btn:nth-child(2)').click();
+				}
+			}
+
+		}, true);
+
+	});
+</script>
+
+<?php
+
+$data = array(
+	"data" => array(),
+	"member_count" => ""
+);
+
+/*
+These 3 sets of data are manipulated to create the member data output.
+
+Custom fields: $fields
+[
+	{"id":"15","plan_id":"0","name":"latitude","title":"Address Latitude","description":"","field_type":null,"required":"0","values":"","default_values":"","rows":"0","cols":"0","size":"0","css_class":"","extra":"","ordering":"15","published":"1","datatype_validation":"0","field_mapping":null,"is_core":"0","show_on_subscription_payment":"0","taxable":"1","newsletter_field_mapping":null,"populate_from_previous_subscription":"1","prompt_text":"","filterable":"0","pattern":null,"min":"0","max":"0","step":"0","show_on_subscription_form":"0","show_on_subscriptions":"0","hide_on_membership_renewal":"0","hide_on_email":"0","hide_on_export":"0","show_on_members_list":"1","show_on_group_member_form":"0","is_searchable":"0","show_on_profile":"0","show_on_user_profile":"1","fee_field":"0","fee_values":"","fee_formula":"","profile_field_mapping":"","depend_on_field_id":"0","depend_on_options":"[]","joomla_group_ids":"","max_length":"0","place_holder":"","multiple":"0","validation_rules":"","server_validation_rules":"","validation_error_message":"","modify_subscription_duration":"","can_edit_on_profile":"1","fieldtype":"Text","populate_from_group_admin":"0","access":"1","filter":"","container_class":"","container_size":"","input_size":"","assignment":"0","allowed_file_types":"","lannguage":null,"fieldSuffix":""},
+	{"id":"16","plan_id":"0","name":"longitude","title":"Address Longitude","description":"","field_type":null,"required":"0","values":"","default_values":"","rows":"0","cols":"0","size":"0","css_class":"","extra":"","ordering":"16","published":"1","datatype_validation":"0","field_mapping":null,"is_core":"0","show_on_subscription_payment":"0","taxable":"1","newsletter_field_mapping":null,"populate_from_previous_subscription":"1","prompt_text":"","filterable":"0","pattern":null,"min":"0","max":"0","step":"0","show_on_subscription_form":"0","show_on_subscriptions":"0","hide_on_membership_renewal":"0","hide_on_email":"0","hide_on_export":"0","show_on_members_list":"1","show_on_group_member_form":"0","is_searchable":"0","show_on_profile":"0","show_on_user_profile":"1","fee_field":"0","fee_values":"","fee_formula":"","profile_field_mapping":"","depend_on_field_id":"0","depend_on_options":"[]","joomla_group_ids":"","max_length":"0","place_holder":"","multiple":"0","validation_rules":"","server_validation_rules":"","validation_error_message":"","modify_subscription_duration":"","can_edit_on_profile":"1","fieldtype":"Text","populate_from_group_admin":"0","access":"1","filter":"","container_class":"","container_size":"","input_size":"","assignment":"0","allowed_file_types":"","lannguage":null,"fieldSuffix":""}
+]
+Custom field values: $this->fieldsData
+{"1605":{"15":"26.0092472","16":"-80.3933008"}}
+...
+
+Member details: $this->items, $row->field in the loop
+[
+	{"id":"1605","plan_id":"1","user_id":"2722","coupon_id":"0","first_name":"Damian","last_name":"Davila","organization":"Moventis","address":"18331 Pines Blvd #121","address2":"","city":"Pembroke Pines","state":"Florida","zip":"33029","country":"United States","phone":"+1,954-319-3567","fax":"","email":"damian@moventisusa.com","comment":"","created_date":"2020-07-10 12:54:59","payment_date":"0000-00-00 00:00:00","from_date":"2017-01-12 00:00:00","to_date":"0000-00-00 00:00:00","published":"1","amount":"0.00","tax_amount":"0.00","discount_amount":"0.00","gross_amount":"0.00","subscription_code":null,"payment_method":"os_paypal","transaction_id":"QNJNSG8L5DI7AXDL","act":"subscribe","from_subscription_id":"0","renew_option_id":"0","upgrade_option_id":"0","first_reminder_sent":"0","second_reminder_sent":"0","process_payment_for_subscription":"0","vies_registered":"0","offline_recurring_email_sent":"0","show_on_members_list":"1","refunded":"0","parent_id":"0","auto_subscribe_processed":"0","is_free_trial":"0","subscribe_newsletter":"1","agree_privacy_policy":"1","mollie_customer_id":null,"mollie_recurring_start_date":null,"tax_rate":"0.000000","trial_payment_amount":"0.000000","payment_amount":"0.000000","payment_currency":null,"receiver_email":null,"avatar":"moventis-logo-square.png","payment_made":"0","params":null,"recurring_profile_id":"","subscription_id":"","recurring_subscription_cancelled":"0","renewal_count":"0","from_plan_id":"0","membership_id":"2722","invoice_year":"0","is_profile":"1","invoice_number":"0","profile_id":"1605","language":"en-GB","username":"Damian","user_password":null,"payment_processing_fee":"0.00","group_admin_id":"0","subscription_end_sent":"0","third_reminder_sent":"0","first_reminder_sent_at":null,"second_reminder_sent_at":null,"third_reminder_sent_at":null,"subscription_end_sent_at":null,"plan_main_record":"1","plan_subscription_status":"1","plan_subscription_from_date":"2017-01-12 00:00:00","plan_subscription_to_date":"0000-00-00 00:00:00","setup_fee":"0.00","gateway_customer_id":null,"ip_address":null,"first_sms_reminder_sent":"0","second_sms_reminder_sent":"0","third_sms_reminder_sent":"0","formatted_invoice_number":null,"formatted_membership_id":null,"plan_title":"AATCVM Board Members"}
+	...
+]
+*/
+
+$custom_field_names = array_column($fields, 'name');
+$latitude_key = array_search('latitude', $custom_field_names);
+$latitude_key = $fields[$latitude_key]->id;
+$longitude_key = array_search('longitude', $custom_field_names);
+$longitude_key = $fields[$longitude_key]->id;
+
+$fieldsData = $this->fieldsData;
+$previous_user_id = '';
+
+for ($i = 0 , $n = count($this->items) ; $i < $n ; $i++)
+{
+	$row = $this->items[$i];
+
+	if ($row->user_id == $previous_user_id)
+	{
+		// Default behavior is to show all subscriptions, but here want only unique subscribers.
+		// Therefore, check for duplicates using Joomla user id which is stored with each subscription.
+		continue;
+	}
+	$previous_user_id = $row->user_id;
+
+	$link = JRoute::_('index.php?option=com_osmembership&view=member&id=' . $row->id . '&Itemid=' . $this->Itemid);
+	$member_data = [];
+	$member_data['id'] = $row->id;
+	$member_data['address'] = $row->address . '<br/>'. $row->address2 . '<br/>' . $row->city .', '. $row->state .' '. $row->zip .' '. $row->country;
+	$member_data['longitude'] = $fieldsData[$row->id][$longitude_key];
+	$member_data['latitude'] = $fieldsData[$row->id][$latitude_key];;
+	$member_data['name'] = $row->first_name . ' ' . $row->last_name;
+	$member_data['email'] = $row->email;
+	$member_data['organization'] = $row->organization;
+	$member_data['profile_marker_picture'] = '';
+	$member_data['member_link'] = $link;
+	$member_data['avatar'] = '';
+	
+	if ($showAvatar)
+	{
+		if ($row->avatar && file_exists(JPATH_ROOT . '/media/com_osmembership/avatars/' . $row->avatar))
+		{
+			$member_data['avatar'] = $row->avatar;
+		}
+	}
+	$data['data'][] = $member_data;
+
+}
+$data['member_count'] = count($data['data']);
+$data = json_encode($data, JSON_HEX_TAG);
+
+?>
+
+<script type="text/javascript">
+
+jQuery(document).ready(function() {
+
+	google.maps.event.addDomListener(window, 'load', showPractitionerMap(<?php echo $data;?>));
+
+});
+
+//var data = <?php echo $data;?>;
+
+</script>
+<style>
+#markerlist {
+	height: 510px;
+	overflow: auto;
+}
+#markerlist div a{
+	margin:0px 0px 0px 15px;
+}
+.btn {
+  box-shadow: 0px 0px 5px #00f3;
+}
+.wrapper {
+  display: grid;
+  gap: 20px;
+  grid-template-areas:
+    "map"
+    "list";
+}
+.filters input {
+  height: auto;
+  width: 100%;
+  box-sizing: border-box;
+  -webkit-box-sizing: border-box;
+  -moz-box-sizing: border-box;
+}
+.osm-map {
+	grid-area: map;
+}
+.osm-list {
+	grid-area: list;
+}
+#osm-members-list .filters {
+	display: grid;
+	grid-template-columns: auto 83px;
+	grid-template-areas: "search-input search-buttons";	
+	gap: 3px;
+}
+#osm-members-list .filters div.filter-search {
+	float: none;
+  	grid-area: search-input;
+}
+#osm-members-list .filters div:not(.filter-search) {
+	float: none;
+	margin-left: 0;
+	grid-area: search-buttons;
+}
+#osm-members-list form {
+	margin-bottom: 0px;
+}
+#osm-map .filters {
+	display: grid;
+	gap: 5px;
+	grid-template-areas: 
+	"map-country"
+	"map-radius"
+	"map-buttons";	
+	margin-bottom: 10px;
+}
+#osm-map .filters .btn {
+  vertical-align: top;
+  width: 92px;
+}
+#osm-map .filters input {
+  margin-bottom: 0px;
+}
+#osm-map .filters #pac-input {
+	grid-area: map-country;
+}
+#osm-map .filters #pac-radius {
+	grid-area: map-radius;
+}
+#osm-map .filters .map-buttons {
+  grid-area: map-buttons;
+}
+.info h3 {
+	margin-bottom: 3px;
+}
+
+@media (min-width: 768px) {
+  .wrapper {
+    grid-template-columns: auto 30%;
+    grid-template-areas:
+      "map list";
+  }
+  #osm-map .filters {
+	display: grid;
+	grid-template-columns: 2fr 1fr 190px;
+	grid-template-areas: "map-country map-radius map-buttons";	
+  }
+}
+</style>	
+
+<h1 class="osm-page-title"><?php echo $this->params->get('page_heading') ?: JText::_('OSM_MEMBERS_LIST') ; ?></h1>
+<section id="osm-members-map-intro" class="osm-container">
+	<div class="intro">
+		<p>Our directory of registered TCVM professionals is intended for use by potential clients to locate veterinarians who can provide TCVM services for their animals or for TCVM practitioners to find and connect with others in their field.</p>
+		<ul>
+			<li>Enter a city, or state, or state + zip code (U.S. only) in the space below and click "Locate" button to find local TCVM practitioners in the area.</li>
+			<li>You may also drag the map to other areas, and use the plus (+) and minus (-) buttons to zoom in or out.</li>
+			<li>Numbered circles represent clusters of multiple practitioners. Click on clusters to expand them and see individual listings.</li>
+		</ul>
+		<p><strong>Registered AATCVM/WATCVM members:</strong> To update your profile information, simply log in first then <a href="index.php?option=com_osmembership&view=profile" title="Access the profile update page">click here to view the profile update page</a>.</p>
+	</div>
+</section>
+<section id="osm-members-map" class="wrapper">
+	<div id="osm-members-list" class="osm-list">
+		<form method="post" name="adminForm" id="adminForm" action="<?php echo JRoute::_('index.php?option=com_osmembership&view=members&Itemid='.$this->Itemid); ?>">
+			<div class="filters">
+				<?php echo $this->loadTemplate('search'); ?>
+			</div>
+			<input type="hidden" name="filter_order" value="<?php echo $this->state->filter_order; ?>" />
+			<input type="hidden" name="filter_order_Dir" value="<?php echo $this->state->filter_order_Dir; ?>" />
+		</form>
+		<div id="markerlist" class=""></div>
+	</div>
+	<div id="osm-map" class="osm-map">
+		<div class="filters">
+			<input id="pac-input" class="controls hasTooltip" type="text" placeholder="City, State, Country" data-original-title="Enter the city, state, and country">
+			<input id="pac-radius" class="controls hasTooltip" id='radius' name='radius' type="number" placeholder="Search radius" data-original-title="Specify the search radius from center of that location">
+			<div class="map-buttons">
+				<input class="btn" value="Locate" onclick="codeAddress()" type="button">
+				<input class="btn" value="Reset Map" onclick="reset_state()" type="button">
+			</div>
+		</div>
+		<div id="map" style="height:500px;"></div>
+	</div>
+</section>
